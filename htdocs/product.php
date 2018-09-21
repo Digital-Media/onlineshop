519c82a6031a2054cdcd85452b64bca87a7ebec2
Martin Harrer <martin.harrer@fh-hagenberg.at>
2018-09-21 17:11:22 +0200

    Update product.php

--- htdocs/product.php
+++ htdocs/product.php
@@ -44,9 +44,5 @@ try {
     $product = new Product($view);
     $product->normForm();
 } catch (Exception $e) {
-    if (DEBUG) {
-        echo "An error occured in file " . $e->getFile() ." on line " . $e->getLine() .":" . $e->getMessage();
-    } else {
-        echo "<h2>Something went wrong</h2>";
-    }
+    echo "<h2>Something went wrong</h2>";
 }
