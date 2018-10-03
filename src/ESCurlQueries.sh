775c021f8006dceba6257f85945dbf25b32f0ed6
Harrer Martin <P20137@fhooe.at>
2018-10-03 09:41:51 +0200

    Removed PUT and added more _analyze endpoints

--- src/ESCurlQueries.sh
+++ src/ESCurlQueries.sh
@@ -31,41 +31,6 @@ curl -X POST "localhost:9200/product_dict_decompounder/_analyze" -H 'Content-Typ
   "text": "Almgrundstueck an einem Bergsee"
 }
 '
-curl -X PUT "localhost:9200/product_hyphen_decompounder/_doc/1" -H 'Content-Type: application/json' -d'
-{
-    "product_name" : "Passivhaus",
-    "short_description" : "Haus mit U-Wert<10kWh/am2",
-    "long_description" : "Haus mit U-Wert<10kWh/am2\r\n20m2 Solaranlage\r\n40m2 Photovoltaik\r\n7500l Regenwassertank, ideal an kalten Wintertagen"
-}
-'
-curl -X PUT "localhost:9200/product_hyphen_decompounder/_doc/2" -H 'Content-Type: application/json' -d '
-{
-    "product_name" : "Niedrigenergiehaus",
-    "short_description" : "Haus mit U-Wert<45kWh/am2",
-    "long_description" : "Haus mit U-Wert<45kWh/am2\r\n20 m2 Solaranlage, ideal an kalten Wintertagen"
-}
-'
-curl -X PUT "localhost:9200/product_hyphen_decompounder/_doc/3" -H "Content-Type: application/json" -d '
-{
-    "product_name" : "Seegrundstück",
-    "short_description" : "Seegrundstück am Attersee",
-    "long_description" : "Seegrundstück am Attersee mit Seeblick und Bergblick, ideal für heiße Sommertage"
-}
-'
-curl -X PUT "localhost:9200/product_hyphen_decompounder/_doc/4" -H 'Content-Type: application/json' -d '
-{
-    "product_name" : "Almgrundstück",
-    "short_description" : "Almgrundstück an einem Bergsee",
-    "long_description" : "Almgrundstück an einem Bergsee mit Zufahrtsstrasse, geschottert und Winterräumung, ideal für heiße Sommertage"
-}
-'
-curl -X PUT "localhost:9200/product_hyphen_decompounder/_doc/5" -H 'Content-Type: application/json' -d '
-{
-    "product_name" : "Talgrundstück",
-    "short_description" : "Grundstück am Talende",
-    "long_description" : "Talgrundstück am Ende des Steyerlingtales, wenig Sonne, dafuer viel kaltes Bachwasser direkt neben dem Grundstück, ideal für heiße Sommertage"
-}
-'
 curl -X GET "localhost:9200/product_hyphen_decompounder/_search" -H 'Content-Type: application/json' -d'
 {
   "from" : 0, "size" : 2,
@@ -88,3 +53,44 @@ curl -X GET "localhost:9200/product_hyphen_decompounder/_search" -H 'Content-Typ
         }
     }
 }}'
+curl -X GET "localhost:9200/product_hyphen_decompounder/_search" -H 'Content-Type: application/json' -d'
+{
+    "query": {
+        "match" : {
+            "search" : {
+                "query" : "haus alm"
+            }
+        }
+    }
+}}'
+curl -XGET "http://localhost:9200/product_hyphen_decompounder/_doc/_search?pretty=true" -H 'Content-Type: application/json' -d '
+{
+    "query" : {
+        "match_all" : {}
+    }
+}
+'
+curl -X POST "localhost:9200/product_hyphen_decompounder/_analyze?pretty=true" -H 'Content-Type: application/json' -d'
+{
+  "field": "product_name",
+  "text": "Reihenhäuser"
+}
+'
+curl -X POST "localhost:9200/product_hyphen_decompounder/_analyze?pretty=true" -H 'Content-Type: application/json' -d'
+{
+  "field": "short_description",
+  "text": "Häuser mit Mehrwert"
+}
+'
+curl -X POST "localhost:9200/product_hyphen_decompounder/_analyze?pretty=true" -H 'Content-Type: application/json' -d'
+{
+  "field": "long_description",
+  "text": "Schöne Reihenhäuser im Grünen mit Blick in die Berge"
+}
+'
+curl -X POST "localhost:9200/product_hyphen_decompounder/_analyze?pretty=true" -H 'Content-Type: application/json' -d'
+{
+  "field": "search",
+  "text": "Schöne Reihenhäuser im Grünen mit Blick in die Berge"
+}
+'
