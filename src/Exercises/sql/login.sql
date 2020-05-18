cfaa5456709cd1d75321fb944a03a45ca6ede5ee
MH <18611980+martinharrer@users.noreply.github.com>
2020-05-18 11:51:39 +0200

    added more statements to SQL file for alternate ER modelling

--- src/Exercises/sql/login.sql
+++ src/Exercises/sql/login.sql
@@ -1,13 +1,16 @@
-GRANT ALL PRIVILEGES ON login.* TO 'onlineshop'@'localhost';
-DROP SCHEMA IF EXISTS login;
-CREATE SCHEMA IF NOT EXISTS `login` DEFAULT CHARACTER SET utf8;
+-- DROP SCHEMA IF EXISTS login;
+-- CREATE SCHEMA IF NOT EXISTS login DEFAULT CHARACTER SET utf8;
+-- GRANT ALL PRIVILEGES ON login.* TO 'onlineshop'@'localhost';
 USE login;
 -- place your solutions here
+
+CREATE TABLE login ( iduser BIGINT UNSIGNED NOT NULL AUTO_INCREMENT , `product_name` VARCHAR(255) NOT NULL , `price` DECIMAL(10,2) NOT NULL , `description` TEXT NOT NULL , `date_added` DATE NOT NULL , PRIMARY KEY (`idproduct`)) ENGINE = InnoDB;
+
 
 
 
 -- end solution
 
-DROP SCHEMA login;
+-- DROP SCHEMA login;
 
-REVOKE ALL PRIVILEGES ON login.* FROM 'onlineshop'@'localhost';
+-- REVOKE ALL PRIVILEGES ON login.* FROM 'onlineshop'@'localhost';
